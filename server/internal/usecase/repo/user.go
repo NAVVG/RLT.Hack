eb2d1adf24ba19509a23aed4fcbb225ae3ad3b97
Vladimir <74735618+qsoulior@users.noreply.github.com>
2023-04-07 22:08:33 +0300

    Добавлены тела функций репозитория User

--- server/internal/usecase/repo/user.go
+++ server/internal/usecase/repo/user.go
@@ -1,6 +1,8 @@
 package repo
 
 import (
+	"context"
+
 	"github.com/NAVVG/test/internal/entity"
 	"github.com/NAVVG/test/pkg/postgres"
 )
@@ -9,11 +11,24 @@ type User struct {
 	postgres *postgres.Postgres
 }
 
-func (u *User) Create(name string, email string, password string) error
+func (u *User) Create(name string, email string, password string) error {
+	query := "insert into users (name, email, password) values ($1, $2, $3)"
+	_, err := u.postgres.Pool.Exec(context.Background(), query, name, email, password)
+	return err
+}
 
-func (u *User) GetByName(name string) (entity.User, error)
+func (u *User) GetById(id int) (entity.User, error) {
+	query := "select * from users where id=$1"
+	var user entity.User
+	err := u.postgres.Pool.QueryRow(context.Background(), query, id).Scan(&user)
+	return user, err
+}
 
-func (u *User) DeleteByName(name string) error
+func (u *User) DeleteById(id int) error {
+	query := "delete from users where id=$1"
+	_, err := u.postgres.Pool.Exec(context.Background(), query, id)
+	return err
+}
 
 func NewUser(pg *postgres.Postgres) *User {
 	return &User{postgres: pg}
