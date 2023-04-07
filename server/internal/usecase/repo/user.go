ce6c61576f11f3c9e4af81a1a9f7dcddfb1f3425
Vladimir <74735618+qsoulior@users.noreply.github.com>
2023-04-07 21:04:17 +0300

    Добавлены заголовки функций репозитория User

--- server/internal/usecase/repo/user.go
+++ server/internal/usecase/repo/user.go
@@ -1,8 +1,20 @@
 package repo
 
+import (
+	"github.com/NAVVG/test/internal/entity"
+	"github.com/NAVVG/test/pkg/postgres"
+)
+
 type User struct {
+	postgres *postgres.Postgres
 }
 
-func NewUser() *User {
-	return &User{}
+func (u *User) Create(name string, email string, password string) error
+
+func (u *User) GetByName(name string) (entity.User, error)
+
+func (u *User) DeleteByName(name string) error
+
+func NewUser(pg *postgres.Postgres) *User {
+	return &User{postgres: pg}
 }
