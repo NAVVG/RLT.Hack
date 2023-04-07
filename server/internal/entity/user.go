e6c5b9d61eabcc8fa7de84fad8f5521620641f36
Vladimir Kochnev <dv16@3soft.ru>
2023-04-07 20:59:44 +0300

    Добавил структуру user

--- server/internal/entity/user.go
+++ server/internal/entity/user.go
@@ -1,4 +1,18 @@
 package entity
 
+import (
+	"time"
+)
+
 type User struct {
+	Id       int
+	Name     string
+	Email    string
+	Password string
+}
+
+type Tokens struct {
+	User    int
+	Token   string
+	ExpDate time
 }
